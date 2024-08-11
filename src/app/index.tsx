b011fe5697185a2c74de251552178a505ef40b41
el0marca <el0mehdie@gmail.com>
2024-08-11 13:34:17 +0400

    fix login

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+localStorage.setItem("token", "0a551067-569c-4b1a-857e-d24c1c0dc9d3");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,19 +1,10 @@
-import React, { useEffect, useMemo } from "react";
+import React, {  useMemo } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import getRoutes from "./routes";
-import {
-  useGetOrUpdateTokenQuery,
-  useLoginByPhoneCallMutation,
-} from "@entities/login";
+import { useGetOrUpdateTokenQuery } from "@entities/login";
 import { LoadingIndicator } from "@shared/ui/loadingIndicator";
 
 const AppRouter: React.FC = () => {
-  const [login] = useLoginByPhoneCallMutation();
-
-  useEffect(() => {
-    login({ phone: "79092991333" });
-  }, []);
-
   const { isFetching } = useGetOrUpdateTokenQuery();
 
   const isAuthorized = true;

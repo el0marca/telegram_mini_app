d57f5b324bfd582146118271fbdb5dfbdb85c0d6
el0marca <el0mehdie@gmail.com>
2024-08-11 15:43:02 +0400

    console

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-
+console.log(window.location.href);
 localStorage.setItem("token", "0a551067-569c-4b1a-857e-d24c1c0dc9d3");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);

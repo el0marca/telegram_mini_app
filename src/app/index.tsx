4fba8ece5595d916a1214783730224746d606a9f
el0marca <el0mehdie@gmail.com>
2024-08-11 15:58:12 +0400

    alert

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,5 +5,5 @@ const url = new URL(window.location.href);
 const mobileToken = url.searchParams.get("mobileToken");
 
 mobileToken && localStorage.setItem("token", mobileToken);
-
+alert(localStorage.getItem("token"))
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
